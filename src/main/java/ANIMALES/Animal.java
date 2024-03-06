47c81ab84b0547959e9f06d5a51b03b727d6f226
Armenteros05 <sarmerod@myuax.com>
2024-03-06 10:59:27 +0100

    Animal

--- src/main/java/ANIMALES/Animal.java
+++ src/main/java/ANIMALES/Animal.java
@@ -5,4 +5,17 @@ public class Animal {
     private String tipo;
     private String estado;
     private String habitat;
+
+public Animal(String nombre, String tipo, String estado, String habitat) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.habitat = habitat;
+    }
+
+    public Animal(String nombre, String acuático, String estado) {
+    }
+    public String monitorearEstado() {
+        return "Monitoreando estado del animal: " + estado;
+    }
 }
