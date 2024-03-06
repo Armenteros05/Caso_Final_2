ca340b0368717c3ae2b022721fcd700f0ec178d5
Armenteros05 <sarmerod@myuax.com>
2024-03-06 10:47:05 +0100

    Creacion de estructura basica del habitat

--- src/main/java/HABITAT/Habitat.java
+++ src/main/java/HABITAT/Habitat.java
@@ -1,4 +1,19 @@
 package HABITAT;
 
 public class Habitat {
+    private String tipo;
+    private double temperatura;
+    private double humedad;
+    private String limpieza;
+
+    public Habitat(String tipo, double temperatura, double humedad, String limpieza) {
+        this.tipo = tipo;
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+        this.limpieza = limpieza;
+    }
+
+    public String monitorearCondiciones() {
+        return "Monitoreando condiciones del hábitat " + tipo + " con temperatura " + temperatura + "°C, humedad " + humedad + "% y limpieza " + limpieza;
+    }
 }
